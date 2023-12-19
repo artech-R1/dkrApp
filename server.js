6a30ef6c570204d3f64df5d13884b2d7f0b284bf
rione <syahriwan80@gmail.com>
2023-12-20 06:15:23 +0700

    update

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 require('./utils/db');
 require('dotenv').config()
 const express = require('express');
-const expressLayouts = require('express-ejs-layouts');
-const router = require('./controllers/usersController');
 const route = { users: require('./routes/user.route'), sign: require('./routes/auth.route') };
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -11,8 +9,6 @@ const bodyParser = require('body-parser')
 
 // Ejs
 app.set('view engine', 'ejs');
-// Ejs Layout
-app.use(expressLayouts);
 // Public Static
 app.use(express.static('public'))
 // parse application/x-www-form-urlencoded
